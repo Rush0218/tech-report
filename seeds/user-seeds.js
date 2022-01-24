e4ea16df647972f990bdcc91c49917cccb120f69
solera2 <rushtre1@gmail.com>
2022-01-23 22:38:22 -0500

    edited styling

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,26 +4,32 @@ const userdata =
     [
         {
             "username": "Seol",
+            "email": "seol@email.com",
             "password": "password"
         },
         {
             "username": "Era",
+            "email": "era@email.com",
             "password": "password"
         },
         {
             "username": "Rye",
+            "email": "rye@email.com",
             "password": "password"
         },
         {
             "username": "Tre",
+            "email": "tre@email.com",
             "password": "password"
         },
         {
             "username": "Flare",
+            "email": "flare@email.com",
             "password": "password"
         },
         {
             "username": "Marcus",
+            "email": "mundy@email.com",
             "password": "password"
         }
     ];
